dac8b84c89cc0f2befb5cf7359acd2ec301e6450
xsitin <m9ckom@gmail.com>
2022-05-23 23:10:04 +0500

    Add wpf client

---
+++ DependentClient/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependentClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            InitializeComponent();
+        }
+    }
+}
