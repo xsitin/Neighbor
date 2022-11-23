43f3618cab658734de2e0ca601e0edaf583a2191
Alex <udovi4@gmail.com>
2022-11-23 22:42:03 +0500

    single img in ad

--- Front/Data/AdsRepository.cs
+++ Front/Data/AdsRepository.cs
@@ -127,9 +127,11 @@ public class AdsRepository
         var request = new HttpRequestMessage(HttpMethod.Post, new Uri(client.BaseAddress, "ads/add"));
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await GetToken());
         var content = new MultipartFormDataContent();
-        content.Add(new StringContent(JsonSerializer.Serialize(ad)), nameof(Ad));
+        content.Add(JsonContent.Create(ad, MediaTypeHeaderValue.Parse("application/json")), nameof(Ad));
+        content.Add(new StreamContent(images[0].OpenReadStream(int.MaxValue)), "Image", images[0].Name);
+        /*content.Add(new StringContent(JsonSerializer.Serialize(ad)), nameof(Ad));
         foreach (var image in images)
-            content.Add(new StreamContent(image.OpenReadStream(int.MaxValue)), image.Name, image.Name);
+            content.Add(new StreamContent(image.OpenReadStream(int.MaxValue)), image.Name, image.Name);*/
         request.Content = content;
         await client.SendAsync(request);
     }
